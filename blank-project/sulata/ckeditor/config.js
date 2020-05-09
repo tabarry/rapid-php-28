7313b2cece8cdf9cd7e2b133620df6700f983f32
tabarry <tahir@sulata.com.pk>
2020-05-09 10:31:36 +0500

    CK Editor Config

--- blank-project/sulata/ckeditor/config.js
+++ blank-project/sulata/ckeditor/config.js
@@ -7,4 +7,5 @@ CKEDITOR.editorConfig = function(config) {
     // Define changes to default configuration here. For example:
     // config.language = 'fr';
     config.uiColor = '#EEEEEE';
+    config.allowedContent = true;
 };
